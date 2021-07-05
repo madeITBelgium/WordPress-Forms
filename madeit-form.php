20505ee892a03e001e749be6ac902822a336da8b
support@madeit.be <info@madeit.be>
2021-07-05 16:12:58 +0000

    Add credists

--- madeit-form.php
+++ madeit-form.php
@@ -5,7 +5,7 @@
  * Description: Build cool, easy and flexible forms with Forms.
  * Author: Made I.T.
  * Author URI: https://www.madeit.be
- * Version: 1.12.3
+ * Version: 1.12.4
  * Text Domain: forms-by-made-it
  * Domain Path: /languages
  * License: GPLv2.
