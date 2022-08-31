a6c938b41519f11b989f24dfe848038d27a6392b
support@madeit.be <info@madeit.be>
2022-08-31 17:06:37 +0000

    Translate

--- gutenberg/input-field/madeit-forms.php
+++ gutenberg/input-field/madeit-forms.php
@@ -2,6 +2,8 @@
 
 function create_block_madeit_forms_input_block_init()
 {
-    register_block_type(__DIR__);
+    register_block_type(__DIR__, [
+        'title' => __("Forms Input Field", "forms-by-made-it"),
+    ]);
 }
 add_action('init', 'create_block_madeit_forms_input_block_init');
--- gutenberg/submit-field/madeit-forms.php
+++ gutenberg/submit-field/madeit-forms.php
@@ -2,6 +2,8 @@
 
 function create_block_madeit_forms_submit_block_init()
 {
-    register_block_type(__DIR__);
+    register_block_type(__DIR__, [
+        'title' => __("Forms Submit Field", "forms-by-made-it"),
+    ]);
 }
 add_action('init', 'create_block_madeit_forms_submit_block_init');
