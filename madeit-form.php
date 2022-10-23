ea0296d4adfd0a98e1297abf0d2200dd01d399c2
Made I.T <info@madeit.be>
2022-10-23 23:31:09 +0200

    Update madeit-form.php

--- madeit-form.php
+++ madeit-form.php
@@ -60,6 +60,8 @@ function madeit_forms_loadActions()
     $a = new WP_MADEIT_FORM_Javacript();
     require_once MADEIT_FORM_DIR.'/actions/Redirect.php';
     $a = new WP_MADEIT_FORM_Redirect();
+    require_once MADEIT_FROM_DIR.'/actions/Klaviyo.php';
+    $a = new WP_MADEIT_FORM_Klaviyo();
 
     require_once MADEIT_FORM_DIR.'/modules/Text.php';
     require_once MADEIT_FORM_DIR.'/modules/Checkbox.php';
